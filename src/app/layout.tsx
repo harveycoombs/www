e9e517e5b80e577a98c464531daf32b6643f78fc
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-12-20 12:55:58 +0000

    Added canonical link and apple touch icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="canonical" href="https://harveycoombs.com" />
+            </head>
             <body className={`h-screen ${inter.className} bg-white text-slate-500`}>
                 <Header />
                 {children}
