16d5fa2881c41f80e15bf291350c7db76d41610a
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-12-20 11:12:47 +0000

    Added noopener to footer link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="p-3">
             <div className="w-1000 flex justify-between items-center mx-auto font-semibold text-slate-400/75 text-sm max-[1050px]:w-full">
                 <div>{new Date().getFullYear()} &middot; Harvey Coombs</div>
-                <div>View this site on <Link href="https://github.com/harveycoombs/www" className="hover:underline">GitHub</Link></div>
+                <div>View this site on <Link href="https://github.com/harveycoombs/www" target="_blank" rel="noopener" className="hover:underline">GitHub</Link></div>
             </div>
         </footer>
     );
