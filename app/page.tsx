be31be066916c90d1db652efe727a14c2881310f
Harvey Coombs <harveycoombs23@outlook.com>
2025-06-26 22:57:12 +0100

    Initial rewrite in Astro

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function Home() {
-    return (
-        <main className="h-full relative overflow-hidden">
-            <h1 className="absolute bottom-0 left-2 uppercase text-9xl leading-none max-2xl:text-8xl max-xl:text-7xl max-lg:bottom-5 max-sm:text-6xl max-sm:leading-20 max-sm:text-center max-sm:inset-0 max-sm:m-auto max-sm:h-fit max-sm:px-4">Harvey Coombs</h1>
-        </main>
-    );
-}
