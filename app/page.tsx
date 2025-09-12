fd80f1994d0d7edb425ca2c1d1bb9ffe0651e375
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-09-12 12:46:08 +0100

    Re-ordered projects list

--- app/page.tsx
+++ app/page.tsx
@@ -45,9 +45,9 @@ export default function Home() {
 
                     <div className="mt-6 flex gap-7 max-md:justify-center max-sm:text-base max-sm:gap-3 max-sm:font-medium">
                         <ProjectLink title="Vesper" url="https://vesper.gg" />
+                        <ProjectLink title="Share.surf" url="https://share.surf" />
                         <ProjectLink title="LlamAccel" url="https://llamaccel.com" />
                         <ProjectLink title="Dark Tensor" url="https://darktensor.ai" />
-                        <ProjectLink title="Share.surf" url="https://share.surf" />
                     </div>
                 </motion.div>
             </section>
