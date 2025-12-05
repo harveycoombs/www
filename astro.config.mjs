a940c27c0e191dad0184b37099a0da75ae7d414c
harveycoombs <harveycoombs23@outlook.com>
2025-12-05 19:51:22 +0000

    Netlify configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,29 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
-    site: "https://harveycoombs.com",
-    output: "static",
-    server: {
-        port: 3000
-    },
-    integrations: [react()],
-    vite: {
-        plugins: [tailwindcss()],
-        optimizeDeps: {
-            include: [
-                "@fortawesome/react-fontawesome",
-                "@fortawesome/fontawesome-svg-core",
-                "@fortawesome/free-brands-svg-icons"
-            ]
-        }
-    },
-    outDir: "/var/www/html"
+  site: "https://harveycoombs.com",
+  output: "static",
+
+  server: {
+      port: 3000
+  },
+
+  integrations: [react()],
+
+  vite: {
+      plugins: [tailwindcss()],
+      optimizeDeps: {
+          include: [
+              "@fortawesome/react-fontawesome",
+              "@fortawesome/fontawesome-svg-core",
+              "@fortawesome/free-brands-svg-icons"
+          ]
+      }
+  },
+
+  outDir: "/var/www/html",
+  adapter: netlify()
 });
