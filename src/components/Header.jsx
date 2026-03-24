8d77eade9df14b68ced8897977d4a6b94e8c04ab
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2026-03-24 19:51:13 +0000

    Animated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
+import { motion } from "motion/react";
 import { faGithub, faRedditAlien, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import SocialMediaIcon from "./SocialMediaIcon.jsx";
 
 export default function Header() {
     return (
-        <header className="p-5 flex justify-center items-center gap-5">
+        <motion.header 
+            className="p-5 flex justify-center items-center gap-5" 
+            initial={{ opacity: 0, y: -50 }} 
+            animate={{ opacity: 1, y: 0 }} 
+            transition={{ duration: 0.6, delay: 2, ease: "easeInOut" }}
+        >
             <SocialMediaIcon icon={faGithub} url="https://github.com/harveycoombs" title="GitHub" />
             <SocialMediaIcon icon={faRedditAlien} url="https://www.reddit.com/user/Harvey-Coombs" title="Reddit" />
             <SocialMediaIcon icon={faLinkedin} url="https://www.linkedin.com/in/harveycoombs" title="LinkedIn" />
-        </header>
+        </motion.header>
     );
 }
